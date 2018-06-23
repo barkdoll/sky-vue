05101dcf496268a8df4d600362e8dc0759ae932c
Jesse Barkdoll <jessebarkdoll@gmail.com>
2018-06-23 14:01:31 -0400

    add sample api config file

---
+++ api-config-sample.js
@@ -0,0 +1,6 @@
+// This app is set up for the Apixu Weather API,
+// but feel free to modify it to work with another Weather API's
+// data structure if you'd like.
+module.exports = {
+  API_KEY: 'insert your API key from Apixu'
+}
